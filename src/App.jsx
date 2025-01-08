a0f3066936f4f94c9ee06c158e2e448a6ee79e22
janWICKEDpro <janroyal45@gmail.com>
2025-01-08 15:07:14 +0100

    installed router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter, Routes, Route } from "react-router";
 import "./App.css";
 
 function App() {
---
+++ src/pages/createBoard.jsx
@@ -0,0 +1,3 @@
+export default function CreateBoard() {
+  return <div></div>;
+}
